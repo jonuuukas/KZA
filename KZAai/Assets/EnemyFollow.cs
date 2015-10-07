ce051d05ba3c60444251bae1be699b3731d0007e
Jonas Sideravičius <jonas.sideravicius@mif.stud.vu.lt>
2015-10-07 17:29:37 +0300

    visi  like failai

---
+++ KZAai/Assets/EnemyFollow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFollow : MonoBehaviour {
+	Transform target;
+	NavMeshAgent navigator;
+	SphereCollider trigger;
+	bool hasTarget = false;
+	// Use this for initialization
+	void Start () {
+		trigger = GetComponent<SphereCollider> ();
+		navigator = GetComponent<NavMeshAgent> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hasTarget) {
+			navigator.destination = target.position;
+		}
+	}
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("Entered");
+		if (col.gameObject.tag == "Player" && hasTarget==false) {
+			target = col.gameObject.transform;
+			hasTarget = true;
+		}
+	}
+}
