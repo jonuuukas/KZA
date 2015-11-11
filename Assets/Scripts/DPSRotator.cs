b017fc5f820a3e03294343acf56b9a8bdb54c672
Jonas Sideravičius <jonas.sideravicius@mif.stud.vu.lt>
2015-11-11 18:47:05 +0200

    rotator script of weapon

---
+++ Assets/Scripts/DPSRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DPSRotator : MonoBehaviour {
+
+
+	Transform sphere;
+	public Transform center;
+	// Use this for initialization
+	void Start () {
+		sphere = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		sphere.RotateAround (center.position, Vector3.up, 90 * Time.fixedDeltaTime);
+	}
+}
