6b23d298985aef15e739aacb4fde051744ddaf93
Jonas Sideravičius <jonas.sideravicius@mif.stud.vu.lt>
2015-12-02 23:58:48 +0200

    figuros sukas, shaderis pakeistas, medziai pasukioti, pakeista sfera besisukiojanti aplink dpsa

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+	public float speed = 15f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate (Vector3.up, speed * Time.fixedDeltaTime);
+	}
+}
