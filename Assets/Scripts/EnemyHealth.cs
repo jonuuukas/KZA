1aaef539667ae95062fc3a8eb50da03db28169d3
Tomas Novikovas <scriverry@gmail.com>
2015-12-05 11:07:54 +0200

    Enemy health plates

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour {
-	public int healthPoints = 100;
+    public int healthPoints;
+    public int maxHealthPoints = 100;
+    private Image healthBar;
 	public static bool isDead = false;
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        healthPoints = maxHealthPoints;
+        healthBar = transform.FindChild("EnemyCanvas").FindChild("HealthBG").FindChild("Health").GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
@@ -14,10 +18,12 @@ public class EnemyHealth : MonoBehaviour {
 		if (healthPoints <= 0) {
 			Destroy (gameObject, 0.5f);
 		}
-	}
+
+    }
 
 	void getDamage(int damagePoints){
 		healthPoints -= damagePoints;
+        healthBar.fillAmount = (float)healthPoints / (float)maxHealthPoints;
 	}
 
 	void OnCollisionEnter(Collision coll){
